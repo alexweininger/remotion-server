2fd6ee3bb3a4874c5a520a35f27d7ceded7d9208
alexweininger <alex.weininger@live.com>
2021-10-28 20:41:13 -0700

    Use Img tag

--- src/templates/PreviewCard.tsx
+++ src/templates/PreviewCard.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import {AbsoluteFill, Img} from 'remotion';
 import './fonts.css';
 
 const fontFamily = 'Inter';
 
 const titleStyle: React.CSSProperties = {
-	fontSize: '5.5em',
+	fontSize: '8.5em',
 	marginTop: 0,
 	fontWeight: 700,
 	marginBottom: 0,
+	zIndex: 10,
 };
 
 const gradientText: React.CSSProperties = {
-	background: 'linear-gradient(to right, black, #666)',
+	background: 'linear-gradient(to right, white, #9146ff)',
 	WebkitBackgroundClip: 'text',
 	WebkitTextFillColor: 'transparent',
 	fontFamily,
@@ -22,12 +24,13 @@ export const PreviewCard: React.FC<{
 	img: string;
 }> = ({title, img}) => {
 	return (
-		<div
-			style={{backgroundImage: `url(${img})`, height: '100%', width: '100%'}}
-		>
+		<>
+			<AbsoluteFill>
+				<Img src={img} />
+			</AbsoluteFill>
 			<div style={titleStyle}>
 				<span style={gradientText}>{title}</span>
 			</div>
-		</div>
+		</>
 	);
 };
